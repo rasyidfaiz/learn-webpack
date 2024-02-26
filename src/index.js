6bc46beee572cd8b7fd623c5a04e052299527367
rasyidfaiz <ir.muhammadfaiz@gmail.com>
2024-02-26 16:35:20 +0700

    Add imports and styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import { run } from './app/app.js';
 import { AlertService } from './app/alert.service.js';
+import { run } from './app/app.js';
 import { CalculatorService } from './app/calculator.service.js';
 import { JokesService } from './app/jokes.service.js';
+import './style.scss';
+import 'bootstrap';
 
 const alertService = new AlertService();
 const calculatorService = new CalculatorService();
