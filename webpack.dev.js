5c825cd661b835495da0e2b93a8934e67dbc6db4
rasyidfaiz <ir.muhammadfaiz@gmail.com>
2024-02-28 11:14:31 +0700

    Add devServer configuration and update output filename and assetModuleFilename

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const config = require('./webpack.config.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(config, {
   mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
+    assetModuleFilename: 'images/[name][ext]',
     clean: true,
   },
-  watch: true,
+  plugins: [new MiniCssExtractPlugin()],
 });
