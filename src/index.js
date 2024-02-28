28c13bd7e7e31b4af47b66f03108c2588f1a68ef
rasyidfaiz <ir.muhammadfaiz@gmail.com>
2024-02-28 11:15:29 +0700

    Add vendor.js import and log message in index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { run } from './app/app.js';
 import { CalculatorService } from './app/calculator.service.js';
 import { JokesService } from './app/jokes.service.js';
 import './style.scss';
-import 'bootstrap';
+import './vendor.js';
 
 const alertService = new AlertService();
 const calculatorService = new CalculatorService();
 const jokesService = new JokesService();
 
 run(alertService, calculatorService, jokesService);
+
+console.log('Hello, world! from index.js');
